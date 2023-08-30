4f3e8f8df55e4675849550e2daea34be7d7bf7f9
Richard Burd <richardburd@yahoo.com>
2023-08-30 21:43:56 +0000

    added max span for body

--- components/Layout.js
+++ components/Layout.js
@@ -8,8 +8,12 @@ const Layout = ({ children }) => {
         <title>Urban Footnotes</title>
         <meta name="description" content="A project presented by Urban Cruise Ship" />
       </Head>
-      <div className='bg-neutral-900'>
-        <div>{children}</div>
+      <div className='bg-neutral-900 bg-opacity-95'>
+        <div className="sm:max-w-screen-lg lg:px-0 lg:max-w-screen-lg mx-auto relative">
+            <div className='bg-neutral-900'>
+                <div>{children}</div>
+            </div>
+        </div>
       </div>
     </>
   );
