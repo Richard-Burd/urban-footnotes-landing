15a4e6d1ab1d1cb95e6544301dc40bd3567783ea
Richard Burd <richardburd@yahoo.com>
2023-08-30 17:53:17 +0000

    Next.js App Initialized

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className='text-5xl text-teal-900'>Hello Urban Footnotes!</div>
+    </main>
+  )
+}
