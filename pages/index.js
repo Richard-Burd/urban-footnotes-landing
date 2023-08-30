0c2bc7ed93c4544d9c0c690f09f9b5d5fb54dc9b
Richard Burd <richardburd@yahoo.com>
2023-08-30 21:49:34 +0000

    John's mission statement added

--- pages/index.js
+++ pages/index.js
@@ -30,19 +30,19 @@ export default function Home() {
 
       <div className='m-20'>
         <center>
-          <div className='roboto-font text-4xl text-stone-300 opacity-60 tracking-wide pb-4'>Thesis</div>
+          <div className='roboto-font text-4xl text-stone-300 opacity-60 tracking-wide pb-4'>Mission Statement</div>
         </center>
         <div className='roboto-font italic text-1xl text-stone-300 pl-10'>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            In service of residents who want to use personal mechanized transportation as little as possible, we strive to provide the most helpful assessment of how well all desired services are provided within easy walking distance of any given address.
           </p>
           <br />
           <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+
           </p>
           <br />
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
           </p>
         </div>
       </div>
