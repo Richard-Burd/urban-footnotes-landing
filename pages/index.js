d87df92921960de8a7ead8c91e789b946fea1fe7
Richard Burd <richardburd@yahoo.com>
2023-08-31 01:29:06 +0000

    thesis up

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-8 ${inter.className}`}
     >
       <div className="flex justify-center">
         <div className="flex">
@@ -22,39 +22,51 @@ export default function Home() {
         </div>
         <div className="flex">
           <div>
-            <h1 className='roboto-font text-7xl text-stone-300 pl-10 opacity-60'>Urban Footnotes</h1>
-            <div className='roboto-font text-2xl text-stone-300 pl-20 tracking-wide'>Comming Soon!</div>
+            <h1 className='roboto-font text-4xl lg:text-7xl text-stone-300 lg:pl-10 opacity-60'>Urban Footnotes</h1>
           </div>
         </div>
       </div>
 
-      <div className='m-20'>
+      <div className='lg:m-20'>
         <center>
-          <div className='roboto-font text-4xl text-stone-300 opacity-60 tracking-wide pb-4'>Mission Statement</div>
+          <div className='roboto-font text-2xl pt-12 lg:pt-2 lg:text-4xl text-stone-300 opacity-60 tracking-wide pb-4'>Mission Statement</div>
         </center>
-        <div className='roboto-font italic text-1xl text-stone-300 pl-10'>
+        <div className='roboto-font italic text-1xl text-stone-300 text-justify lg:text-left lg:mx-32'>
           <p>
-            In service of residents who want to use personal mechanized transportation as little as possible, we strive to provide the most helpful assessment of how well all desired services are provided within easy walking distance of any given address.
+            We want to create the best walkability assessment for a given property or location.  Our primary customers are people looking for a place to live who also want to reduce their use of, and dependence on, cars and other transportation means.  Our secondary customers are realtors, real estate developers and others involved in deciding where people live, work, shop, and play.
           </p>
           <br />
           <p>
-
+            Our objective is to slow the rate of urban sprawl through highlighting the benefits of living in more concentrated locations.  We seek to encourage the better use of those locations through encouraging development of mixed use properties which would reduce the need for personal car ownership.
           </p>
           <br />
           <p>
-
+            Urban Footnotes was created by researchers at Urban Cruise Ship in response to their research on 
+            <a
+              href="https://www.urbancruiseship.org/cities"
+              className="duration-500 hover:text-red-900 hover:underline focus:text-red-900 focus:underline text-blue-400 transition"
+              target="_blank"
+              rel="noreferrer"
+            >
+            &nbsp;
+            <b>
+              cities
+            </b>
+            &nbsp;
+          </a>.   We are currently working with early adapters in the real estate industry and plan to develop an online application for general use in the future.
           </p>
         </div>
       </div>
 
+      
       <Link href="https://urbancruiseship.org/">
-        <div className="flex justify-center items-center m-20">
-          <div className="flex">
-            <p className='ucs-font tracking-wider text-2xl text-right text-stone-300 pl-10 opacity-75'>
+        <div className="lg:flex justify-center lg:m-20 mt-24">
+          <div className="lg:flex">
+            <p className='ucs-font tracking-wider text-2xl lg:text-right text-stone-300 lg:pl-10 opacity-75 my-6'>
               A project of Urban Cruise Ship
             </p>
           </div>
-          <div className="flex pl-8">
+          <div className="lg:flex lg:pl-8 justify-center">
             <Image 
               src="/ucs-logo-light.svg"
               alt="Urban Cruise Ship Logo"
