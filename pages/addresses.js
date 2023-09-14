c7949f0c5b6bd41a1f29b27cd0a1cf2459837db7
Richard Burd <richardburd@yahoo.com>
2023-09-14 00:39:56 +0000

    Cloudinary implemented & working

--- pages/addresses.js
+++ pages/addresses.js
@@ -12,7 +12,19 @@ export default function Addresses() {
           <div className="roboto-font text-2xl pt-12 lg:pt-2 lg:text-4xl text-stone-300 opacity-60 tracking-wide pb-4">
             Addresses
           </div>
+
+          {/* Add your image here */}
+          <div className="my-8">
+            <img
+              src={`${process.env.CLOUDINARY_BASE_URL}/title-page-1015-elvira.png`}
+              alt="Urban Foot Notes Image"
+              width={200}
+              height={400}
+              className="mx-auto"
+            />
+          </div>
         </center>
+
         <div className="roboto-font text-1xl text-stone-300 mx-4 lg:mx-32">
           <p>1015 Elvira St.</p>
           <br />
