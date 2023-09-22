3681f311b18cb24b4b0aa7737e1d920addd6be97
Richard Burd <richardburd@yahoo.com>
2023-09-22 21:08:22 +0000

    UFN title color same as UCS Logo

---
+++ components/PageTitle.js
@@ -0,0 +1,14 @@
+// #d4c0a2
+
+export default function PageTitle() {
+  return (
+    <div className="flex justify-center">
+      <h1
+        style={{ color: "#d4c0a2" }}
+        className="text-center mt-20 roboto-font text-3xl lg:text-7xl lg:pl-10 opacity-80"
+      >
+        Urban Foot Notes
+      </h1>
+    </div>
+  );
+}
--- pages/references.js
+++ pages/references.js
@@ -1,12 +1,6 @@
 export default function Associations() {
   return (
     <div>
-      <div className="flex justify-center">
-        <h1 className="text-center mt-20 roboto-font text-4xl lg:text-7xl text-stone-300 lg:pl-10 opacity-60">
-          Urban Foot Notes
-        </h1>
-      </div>
-
       <div className="lg:m-20">
         <center>
           <h2 className="roboto-font text-2xl pt-12 lg:pt-2 lg:text-4xl text-stone-300 opacity-60 tracking-wide pb-4">
