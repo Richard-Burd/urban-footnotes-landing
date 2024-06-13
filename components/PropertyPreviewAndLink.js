375070c6b8e133a440700bde3894aa7ba6671f90
richardburd <rick.a.burd@gmail.com>
2024-06-13 14:02:47 -0700

    aws image server implemented

--- components/PropertyPreviewAndLink.js
+++ components/PropertyPreviewAndLink.js
@@ -1,17 +1,11 @@
 import React from "react";
 
-const PropertyPreviewAndLink = ({
-  ranking,
-  address1,
-  address2,
-  href,
-  imageName,
-}) => {
+const image = process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL;
+
+const PropertyPreviewAndLink = ({ ranking, address1, href, imageName }) => {
   return (
     <div id={address1} className="mb-4">
-      <div className="text-slate-300 text-1xl text-center">{ranking}</div>
-      {/* <div className="text-slate-300 text-1xl text-center">{address1}</div>
-      <div className="text-slate-300 text-1xl text-center">{address2}</div> */}
+      <div className="text-1xl text-center text-slate-300">{ranking}</div>
       <div className="flex justify-center">
         <div style={{ width: "200px", height: "300px" }}>
           <a href={href} target="_blank" rel="noopener noreferrer">
@@ -25,7 +19,7 @@ const PropertyPreviewAndLink = ({
                   height={400}
                 />
                 <img
-                  className="absolute z-20 transition-opacity duration-300 ease-in-out opacity-40 hover:opacity-90 focus:opacity-100 transform origin-center hover:scale-105"
+                  className="absolute z-20 origin-center transform opacity-40 transition-opacity duration-300 ease-in-out hover:scale-105 hover:opacity-90 focus:opacity-100"
                   src="/images/pdf-preview-pointy-hand.svg"
                   alt="Pointy Hand Preview"
                   width={200}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ["ufn-website-images-and-documents.s3.us-east-2.amazonaws.com"],
+  },
   env: {
     CLOUDINARY_BASE_URL: process.env.CLOUDINARY_BASE_URL,
   },
