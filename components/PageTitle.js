89940228da97a60f56f214fa45bbac5e23286f60
richardburd <rick.a.burd@gmail.com>
2024-06-13 17:49:36 -0700

    green title & subtitle + renaming

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,16 +1,9 @@
-import Image from "next/image";
-
-export default function PageTitle() {
+export default function PageTitle({ topTitle, textColor }) {
   return (
-    <div className="flex justify-center mt-20 mx-4">
-      <Image
-        src="/images/ufn-logo-light.svg"
-        alt="Urban Cruise Ship Logo"
-        width={500}
-        height={164.65}
-        unoptimized={true}
-        style={{ minWidth: "200px", opacity: 0.7 }}
-      />
+    <div
+      className={`roboto-font pb-8 text-center text-6xl tracking-wide ${textColor}`}
+    >
+      {topTitle}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "green-title": "#b5f5a5ff", // Your custom color value
       },
-      listStyleType: {
-        square: "square",
+      fontSize: {
+        "2.5xl": ["1.65rem", { lineHeight: "2.3rem" }],
       },
     },
   },
