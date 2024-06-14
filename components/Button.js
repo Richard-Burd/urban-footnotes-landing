52f588b2d4e601f8603406e1b9f29661f7abad69
richardburd <rick.a.burd@gmail.com>
2024-06-14 11:37:30 -0700

    desktop navbar colors & shadows working

--- components/Button.js
+++ components/Button.js
@@ -1,11 +1,12 @@
-const Button = ({ title, bgColor }) => {
-  return (
-    <button
-      className={`${bgColor} roboto-font text-xl font-bold mx-1 text-neutral-300 py-2 px-4 rounded focus:outline-none`}
-    >
-      {title}
-    </button>
-  );
-};
+// // Currently, this is not used & is slated for deletion
+// const Button = ({ title, bgColor }) => {
+//   return (
+//     <button
+//       className={`${bgColor} roboto-font text-xl font-bold mx-1 text-neutral-300 py-2 px-4 rounded focus:outline-none`}
+//     >
+//       {title}
+//     </button>
+//   );
+// };
 
-export default Button;
+// export default Button;
