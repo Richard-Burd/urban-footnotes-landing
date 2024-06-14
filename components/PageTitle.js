ac2af096af83183ea5bd0ea55a3009a7c6ff5d7f
richardburd <rick.a.burd@gmail.com>
2024-06-13 18:17:12 -0700

    title sizes & title colors up

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,7 +1,7 @@
 export default function PageTitle({ topTitle, textColor }) {
   return (
     <div
-      className={`roboto-font pb-8 text-center text-6xl tracking-wide ${textColor}`}
+      className={`roboto-font text-6.5xl pb-8 text-center tracking-wide ${textColor}`}
     >
       {topTitle}
     </div>
