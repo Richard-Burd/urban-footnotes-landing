f0c0823fdb0c97f5de08278ee756b0ee6688a52e
richardburd <rick.a.burd@gmail.com>
2024-06-20 16:40:11 -0700

    page title margins dynamic

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,6 +1,6 @@
 export default function PageTitle({ topTitle, textColor, shadowColor }) {
   return (
-    <div className="mx-3 mt-10">
+    <div className="mx-3 mt-10 lg:mx-0">
       <div
         className={`${shadowColor} page-title-shadow roboto-font text-6.3xl sm:text-6.4xl pb-8 text-center tracking-wide lg:text-6.5xl ${textColor}`}
       >
