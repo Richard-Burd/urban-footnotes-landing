76ccbf9d458b2385f3a67a0a1a75877bf8bc1afb
richardburd <rick.a.burd@gmail.com>
2024-06-27 18:00:35 -0700

    stripe client-side-test-buy-button

--- pages/order.js
+++ pages/order.js
@@ -33,6 +33,20 @@ export default function Order() {
         </div>
       </div>
       <ContactUsForm />
+      <div className="roboto-font mx-4 mt-20 text-xl text-stone-300 lg:mx-32">
+        <p className="pb-4">
+          You can buy a test version of our product and checkout securely at the
+          link below:
+        </p>
+        <script async src="https://js.stripe.com/v3/buy-button.js"></script>
+
+        <div className="flex items-center justify-center">
+          <stripe-buy-button
+            buy-button-id="buy_btn_1PWSnGGN52GuXWVWQzelGuJO"
+            publishable-key="pk_test_51PWM5ZGN52GuXWVWTnkncuyRJtaxnWSX4w1zdn16pDyuS3biGYEpjz1YjkboYGle8e9gzX9vXg2HB2YjGIEFaKAq00Ss702D0z"
+          ></stripe-buy-button>
+        </div>
+      </div>
     </div>
   );
 }
