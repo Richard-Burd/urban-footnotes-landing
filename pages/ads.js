7317b52543ec57d0df7dbdc5576cc4512d8dfd96
richardburd <rick.a.burd@gmail.com>
2024-07-01 09:27:25 -0700

    Advertisements title on Ads Page

--- pages/ads.js
+++ pages/ads.js
@@ -1,10 +1,18 @@
 import PageHeaderLogo from "@/components/PageHeaderLogo";
+import PageTitle from "@/components/PageTitle";
 
 export default function Ads() {
   return (
     <div>
       <PageHeaderLogo />
       <div className="lg:m-20">
+        <center>
+          <PageTitle
+            topTitle="Advertisements"
+            textColor="text-title-text-ads"
+            shadowColor={`title-shadow-ads`}
+          />
+        </center>
         <center>
           <div className="roboto-font pb-4 pt-12 text-2xl tracking-wide text-stone-300 opacity-60 lg:pt-2 lg:text-4xl">
             Comming Soon...
