ccf6e3ac1961bc02d68ac4d4a0de063fb2e69e08
jyeart <96083227+jyeart@users.noreply.github.com>
2024-11-20 14:26:21 +0100

    contact page

--- pages/contact.js
+++ pages/contact.js
@@ -18,17 +18,13 @@ export default function Contact() {
         </center>
         <div className="roboto-font mx-4 text-xl text-stone-300 lg:mx-32">
           <p className="pb-4">
-            If you are interested in ordering our product as an early adopter,
-            please fill in the information below and we will get back to you
-            within a timely manner.
+          You can reach out to us directly at:{" "}
+            <Link href="mailto: contact@urbanfootnotes.org">
+              contact@urbancruiseship.org
+            </Link>{" "}
           </p>
           <p className="pb-4">
-            You can reach our to Richard directly at:{" "}
-            <Link href="mailto: richard@urbancruiseship.org">
-              richard@urbancruiseship.org
-            </Link>{" "}
-            to get more information on becoming an early adopter. Thank you for
-            taking an interest in Urban Foot Notes!
+            Or you can use our contact form, and one of our team members will get in touch with you.
           </p>
         </div>
       </div>
