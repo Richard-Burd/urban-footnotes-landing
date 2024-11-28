8af59c5406f513520091ed51dac8385ba3146f2b
jyeart <96083227+jyeart@users.noreply.github.com>
2024-11-29 00:16:58 +0100

    Jye safe dev (#12)
    * env variables safely working and loaded
    
    * layout modifications, team page reworking
    
    * next public filestack name change to expose api key to client

--- components/Layout.js
+++ components/Layout.js
@@ -3,25 +3,24 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="bg-neutral-900 bg-opacity-95 min-h-screen flex flex-col">
+    <div className="flex min-h-screen flex-col bg-neutral-900 bg-opacity-95">
       {/* Navbar */}
       <div className="bg-neutral-900">
         <Navbar />
-      
 
-      {/* Main Content */}
-      <main
-          className="flex flex-col items-center mx-auto w-full max-w-screen-xl px-2 sm:px-4 lg:px-6"
-        role="main"
-      >
-        {children}
-      </main>
+        {/* Main Content */}
+        <main
+          className="mx-auto flex w-full max-w-screen-xl flex-col items-center px-2 sm:px-4 lg:px-6"
+          role="main"
+        >
+          {children}
+        </main>
 
-      {/* Footer */}
-      <footer className="bg-neutral-950">
-        <Footer />
-      </footer>
-    </div>
+        {/* Footer */}
+        <footer className="bg-neutral-950">
+          <Footer />
+        </footer>
+      </div>
     </div>
   );
 };
