d79beef11064c728644041448a27c9ae432629a3
jyeart <96083227+jyeart@users.noreply.github.com>
2024-11-29 00:16:58 +0100

    Jye safe dev (#12)
    * env variables safely working and loaded
    
    * layout modifications, team page reworking
    
    * next public filestack name change to expose api key to client

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,8 +1,8 @@
 export default function PageTitle({ topTitle, textColor, shadowColor }) {
   return (
-    <div className="mt-4 lg:mt-4">
+    <div className="m-4 lg:mt-4">
       <div
-        className={`${shadowColor} page-title-shadow roboto-font text-6.3xl sm:text-6.4xl pb-8 text-center tracking-wide lg:text-5.5xl ${textColor}`}
+        className={`${shadowColor} page-title-shadow roboto-font pb-8 text-center text-4xl tracking-wide lg:pb-4 lg:text-5xl ${textColor}`}
       >
         {topTitle}
       </div>
