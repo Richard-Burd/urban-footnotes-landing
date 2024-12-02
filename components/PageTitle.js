52f68c19b679e4b9a98ce06a15b4cc2b8f31d0ac
jyeart <96083227+jyeart@users.noreply.github.com>
2024-12-02 19:13:59 +0000

    morning conversation

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -9,21 +9,23 @@ export default function PageTitle({
   return (
     <div className="m-4 mb-8 flex justify-center lg:mx-16 lg:mb-16">
       {/* Wrapper for logo and text */}
-      <div className="relative flex w-full max-w-[1200px] flex-col items-center sm:flex-row sm:items-end">
+      <div className="relative flex w-full max-w-[1200px] items-center">
         {/* Logo */}
         {showLogo && (
-          <div className="mb-4 flex w-full flex-shrink-0 justify-center pr-4 sm:mb-0 sm:w-1/2 sm:justify-end">
+          <div className="flex flex-shrink-0 items-center pr-4">
             <PageHeaderLogo />
           </div>
         )}
 
         {/* Title */}
         <div
-          className={`w-full pl-0 text-center sm:w-1/2 sm:pl-4 sm:text-left ${textColor} ${shadowColor} page-title-shadow roboto-font text-4xl tracking-wide lg:text-5xl`}
+          className={`flex-grow ${textColor} ${shadowColor} page-title-shadow roboto-font pb-0 text-4xl tracking-wide sm:text-5xl lg:text-6.5xl`}
           style={{
-            lineHeight: "1",
             display: "flex",
-            alignItems: "flex-end",
+            alignItems: "flex-end", // Aligns text to the bottom
+            justifyContent: "flex-start", // Aligns text to the left
+            lineHeight: "1",
+            height: "100%", // Ensures container spans full height
           }}
         >
           {topTitle}
