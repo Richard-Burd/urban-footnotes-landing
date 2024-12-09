e64022ac386398e072f08eea0a6242a1a98bf79e
jyeart <96083227+jyeart@users.noreply.github.com>
2024-12-09 07:40:28 +0000

    deployment test

--- pages/form.js
+++ pages/form.js
@@ -2,13 +2,12 @@ import React, { useState } from "react";
 import * as filestack from "filestack-js";
 
 export default function SupplementaryForm() {
+
   
-  const filestackApiKey = "AhJtHA69ATCK7bfBTgnbzz";
-  
-  //const filestackApiKey = process.env.NEXT_PUBLIC_FILESTACK; // Use environment variable
-/*   if (!process.env.NEXT_PUBLIC_FILESTACK) {
+  const filestackApiKey = process.env.NEXT_PUBLIC_FILESTACK; // Use environment variable
+  if (!process.env.NEXT_PUBLIC_FILESTACK) {
     throw new Error("filestack api key is not defined!");
-  }*/
+
   const client = filestack.init(filestackApiKey); 
 
   // State for form inputs
