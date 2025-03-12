4bc6adf31ff3751cf4c12c47afdbf19146944806
richardburd <rick.a.burd@gmail.com>
2025-03-11 18:17:11 -0700

    samples navbar now mobile friendly

--- components/SamplesNavbarButton.js
+++ components/SamplesNavbarButton.js
@@ -2,7 +2,7 @@ const SamplesNavbarButton = ({ title, bgColor, isActive }) => {
   return (
     <div className={`${isActive ? bgColor : "bg-inherit"} px-2 py-4`}>
       <button
-        className={`${isActive ? "navbar-button-shadow" : ""} roboto-font mx-1 rounded-[11px] bg-[#103a15ff] px-4 py-2 text-left text-[20px] font-bold text-[#b5f5a5ff] focus:outline-none`}
+        className={`${isActive ? "navbar-button-shadow" : ""} roboto-font mx-1 rounded-[11px] bg-[#103a15ff] px-4 py-2 text-[20px] font-bold text-[#b5f5a5ff] focus:outline-none min-[530px]:text-left`}
       >
         {title}
       </button>
