91320d356829970f01b6955dadbd93844f81eb70
richardburd <rick.a.burd@gmail.com>
2025-03-12 22:22:26 -0700

    gallery stylings per John

--- pages/gallery-mid-range-aerial.js
+++ pages/gallery-mid-range-aerial.js
@@ -5,6 +5,12 @@ export default function GalleryMidRangeAerial() {
   return (
     <div>
       <SamplesHeader />
+      <div
+        id="page-title"
+        className="pb-4 pt-9 text-center text-[30px] text-title-text-samples sm:text-[40px] md:text-[50px]"
+      >
+        Gallery of Mid-Range Aerial Illustrations
+      </div>
       <Gallery
         title="Gallery of Mid-Range Aerial Illustrations"
         viewType="aerialMidRange"
