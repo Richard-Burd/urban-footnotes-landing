28fd546ac0bbf97e6656e77f04836f08ab73cec4
richardburd <rick.a.burd@gmail.com>
2025-11-24 10:12:19 -0800

    page numbering updated on Coral Gables

--- pages/samples.js
+++ pages/samples.js
@@ -35,7 +35,7 @@ export default function Samples() {
             serviceTypesFarther={0}
             serviceTypesDistant={20}
             walkabilityScore={26}
-            pdfPath="doc-coral-gables-fl-development-proposal.3.pdf"
+            pdfPath="doc-coral-gables-fl-development-proposal.4.pdf"
           />
           <ProductSample
             imagePath="preview-sarphatipark-38i-amsterdam.3.2.jpg"
