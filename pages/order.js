44e9f1de1d3efb1cd04c6cb73717e9c4d6ef6b05
richardburd <rick.a.burd@gmail.com>
2026-04-30 21:42:33 -0700

    order page complete sans price estimates

--- pages/order.js
+++ pages/order.js
@@ -2,6 +2,60 @@ import { useEffect } from "react";
 import PageTitle from "@/components/PageTitle";
 import Image from "next/image";
 
+function DevelopmentProposalImages({ baseUrl, id = "images", className }) {
+  return (
+    <div id={id} className={className}>
+      <div id="image-1-container" className="mb-28">
+        <div id="image-1-image">
+          <Image
+            alt="high fidelity rendering of Indy dev proposal"
+            src={`${baseUrl}/indy-order-page.1.jpg`}
+            width={450}
+            height={1}
+          />
+        </div>
+        <div id="image-1-text" className="text-xl text-white">
+          <div className="pt-1 italic">
+            high fidelity report available{" "}
+            <a
+              href="https://ufn-website-images-and-documents.s3.us-east-2.amazonaws.com/doc-indianapolis-in-development-proposal.6.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              here
+            </a>
+          </div>
+          <div>estimated cost: $x,xxx</div>
+        </div>
+      </div>
+
+      <div id="image-2-container">
+        <div id="image-2-image">
+          <Image
+            alt="low fidelity rendering of Coral Gables dev proposal"
+            src={`${baseUrl}/coral-gables-order-page.1.jpg`}
+            width={450}
+            height={1}
+          />
+        </div>
+        <div id="image-2-text" className="text-xl text-white">
+          <div className="pt-1 italic">
+            high fidelity report available{" "}
+            <a
+              href="https://ufn-website-images-and-documents.s3.us-east-2.amazonaws.com/doc-coral-gables-fl-development-proposal.7.2.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              here
+            </a>
+          </div>
+          <div>estimated cost: $x,xxx</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Order() {
   const baseUrl = process.env.NEXT_PUBLIC_S3_BASE_URL;
   useEffect(() => {
@@ -86,64 +140,103 @@ export default function Order() {
         id="development-proposals-intro"
         className="w-full max-w-screen-lg px-4 pb-8 text-white"
       >
-        <div className="pb-1 text-2xl font-semibold min-[1000px]:text-4xl min-[1000px]:underline">
+        <div
+          id="title"
+          className="pb-1 text-2xl font-semibold min-[1000px]:text-4xl min-[1000px]:underline"
+        >
           Development Proposals
         </div>
-        <div id="development-proposal-paragraphs">
-          <div id="dev-paragraph-1" className="text-xl min-[1000px]:text-2xl">
-            These reports explore a potential building construction development
-            design for a given parcel or parcel cluster. The reports show how a
-            property could be developed so as to maximize walkability for
-            residents living in the immediate vicinity. These designs are almost
-            always mixed-use complexes with residential, office, and retail
-            spaces.
-          </div>
 
+        <div
+          id="dev-proposal-main-body"
+          className="flex flex-col min-[1000px]:flex-row min-[1000px]:items-start min-[1000px]:gap-8"
+        >
           <div
-            id="dev-paragraph-2"
-            className="mt-8 text-xl min-[1000px]:text-2xl"
+            id="text-paragraphs"
+            className="min-[1000px]:min-w-0 min-[1000px]:flex-1"
           >
-            The cost of a development proposal depends on the development's
-            size, complexity, and level of detail for both design & analysis.
-            For this reason we cannot quote a fixed price but we offer the
-            following examples on the right with estimated costs for each one.
-          </div>
+            <div id="dev-paragraph-1" className="text-xl min-[1000px]:text-2xl">
+              These reports explore a potential building construction
+              development design for a given parcel or parcel cluster. The
+              reports show how a property could be developed so as to maximize
+              walkability for residents living in the immediate vicinity. These
+              designs are almost always mixed-use complexes with residential,
+              office, and retail spaces.
+            </div>
 
-          <div
-            id="dev-paragraph-3"
-            className="mt-8 text-xl min-[1000px]:text-2xl"
-          >
-            Development proposals should be thought of as a tool for parcel
-            optimization & discovery. They can be used to make a case for
-            rezoning, conditional use permit, or a mixed-use overlay. City
-            planners may be more flexible on granting these if a case can be
-            made for an overall public good, which development proposals aim to
-            demonstrate.
-          </div>
+            <div
+              id="dev-paragraph-2"
+              className="mt-8 text-xl min-[1000px]:text-2xl"
+            >
+              The cost of a development proposal depends on the development's
+              size, complexity, and level of detail for both design & analysis.
+              For this reason we cannot quote a fixed price but we offer the
+              following examples on the right with estimated costs for each one.
+            </div>
 
-          <div
-            id="dev-paragraph-4"
-            className="mt-8 text-xl min-[1000px]:text-2xl"
-          >
-            Our development proposals have a level of planning detail
-            commensurate with earlystage preliminary architectural design
-            drawings. They show schematic floor plans broken down by occupancy
-            type per floor. Basic IBC compliant egress schemas are included as
-            well. We can provide varying levels of fidelity in terms of 3D
-            modeling and presentation to meet your specific needs.
-          </div>
+            <div
+              id="mobile-image-placement"
+              className="block pb-20 pt-4 min-[1000px]:hidden"
+            >
+              <DevelopmentProposalImages baseUrl={baseUrl} />
+            </div>
 
+            <div
+              id="dev-paragraph-3"
+              className="mt-8 text-xl min-[1000px]:text-2xl"
+            >
+              Development proposals should be thought of as a tool for parcel
+              optimization & discovery. They can be used to make a case for
+              rezoning, conditional use permit, or a mixed-use overlay. City
+              planners may be more flexible on granting these if a case can be
+              made for an overall public good, which development proposals aim
+              to demonstrate.
+            </div>
+
+            <div
+              id="dev-paragraph-4"
+              className="mt-8 text-xl min-[1000px]:text-2xl"
+            >
+              Our development proposals have a level of planning detail
+              commensurate with earlystage preliminary architectural design
+              drawings. They show schematic floor plans broken down by occupancy
+              type per floor. Basic IBC compliant egress schemas are included as
+              well. We can provide varying levels of fidelity in terms of 3D
+              modeling and presentation to meet your specific needs.
+            </div>
+
+            <div
+              id="dev-paragraph-5"
+              className="mt-8 text-xl min-[1000px]:text-2xl"
+            >
+              Our team has extensive experience in both architecture/ building
+              construction as well as environmental & urban solutions. We
+              leverage this experience to deliver a unique product. That said,
+              we may rely on you for certain due-diligence requirements unique
+              to your city or region. If you have a property address or parcel
+              you would like us to look at, please feel free to contact us so we
+              can discuss your project.
+            </div>
+
+            <div
+              id="dev-paragraph-6"
+              className="mt-8 text-xl min-[1000px]:text-2xl"
+            >
+              <div className="max-[1000px]:mx-0">
+                You can reach out to us at:
+              </div>
+              <div className="max-[1000px]:mx-0">
+                <a href="mailto:contact@urbanfootnotes.com">
+                  contact@urbanfootnotes.com
+                </a>
+              </div>
+            </div>
+          </div>
           <div
-            id="dev-paragraph-5"
-            className="mt-8 text-xl min-[1000px]:text-2xl"
+            id="desktop-image-placement"
+            className="hidden min-[1000px]:block min-[1000px]:shrink-0"
           >
-            Our team has extensive experience in both architecture/ building
-            construction as well as environmental & urban solutions. We leverage
-            this experience to deliver a unique product. That said, we may rely
-            on you for certain due-diligence requirements unique to your city or
-            region. If you have a property address or parcel you would like us
-            to look at, please feel free to contact us so we can discuss your
-            project.
+            <DevelopmentProposalImages baseUrl={baseUrl} />
           </div>
         </div>
       </div>
@@ -153,6 +246,9 @@ export default function Order() {
       <div className="roboto-font w-full max-w-screen-lg px-4 pb-8 text-xl text-white md:text-2xl">
         <div id="explanations-n-pie-chart">
           <div className="roboto-font mb-4 text-xl text-gray-100 md:text-2.5xl">
+            <div className="pb-1 text-2xl font-semibold min-[1000px]:text-4xl min-[1000px]:underline">
+              Costs, Coverage & Accuracy
+            </div>
             <p className="mb-10">
               The graphic below is an approximate percentage breakdown of our
               expenses so far, at present, and going forward (estimated),
